c8725476ce42c9238aa41e485b582eafdeb282d3
Ilya Zlobintsev <ilya.zlve@gmail.com>
2020-08-06 09:45:14 +0300

    Added mute toggle

--- .local/bin/toggle-mute.sh
+++ .local/bin/toggle-mute.sh
@@ -1,1 +1,9 @@
-amixer set Capture toggle && amixer get Capture | grep '\[off\]' && notify-send.sh -t 1000 "MIC muted" --replace-file=/tmp/mutenotif || notify-send.sh -t 1000 "MIC unmuted" --replace-file=/tmp/mutenotif
+amixer set Capture toggle
+MUTE_STATUS="$(amixer get Capture | grep '\[off\]')"
+if [ -n "$MUTE_STATUS" ]; then
+    notify-send.sh -t 1000 "MIC muted" --replace-file=/tmp/mutenotif 
+    play ~/Documents/Mutesound.mp3
+else
+    notify-send.sh -t 1000 "MIC unmuted" --replace-file=/tmp/mutenotif 
+    play ~/Documents/Unmutesound.mp3
+fi
