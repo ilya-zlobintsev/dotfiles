eaf31f9a18e29fe4a000240c39b14ce6d16eea5f
Ilya Zlobintsev <ilya.zl@protonmail.com>
2020-11-03 18:10:11 +0200

    Removed grep

--- .config/fish/functions/grep.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function grep --wraps='grep -RIn' --wraps='grep -RIn --color=always' --description 'alias grep grep -RIn --color=always'
- command grep -RIn --color=always $argv;
-end
